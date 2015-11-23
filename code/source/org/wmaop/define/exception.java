284b3ea8e20c2c882cc0783593e5dfa46d8ae466
richard <richard@sysgen.co.uk>
2015-11-23 17:24:24 +0000

    Updated to manage stub services

---
+++ code/source/org/wmaop/define/exception.java
@@ -0,0 +1,42 @@
+package org.wmaop.define;
+
+// -----( IS Java Code Template v1.2
+// -----( CREATED: 2015-10-12 16:13:19 BST
+// -----( ON-HOST: WSII
+
+import com.wm.data.*;
+import com.wm.util.Values;
+import com.wm.app.b2b.server.Service;
+import com.wm.app.b2b.server.ServiceException;
+// --- <<IS-START-IMPORTS>> ---
+import org.wmaop.flow.MockManager;
+// --- <<IS-END-IMPORTS>> ---
+
+public final class exception
+
+{
+	// ---( internal utility methods )---
+
+	final static exception _instance = new exception();
+
+	static exception _newInstance() { return new exception(); }
+
+	static exception _cast(Object o) { return (exception)o; }
+
+	// ---( server methods )---
+
+
+
+
+	public static final void registerExceptionMock (IData pipeline)
+        throws ServiceException
+	{
+		// --- <<IS-START(registerExceptionMock)>> ---
+		// @sigtype java 3.5
+		new MockManager().registerException(pipeline);
+		// --- <<IS-END>> ---
+
+                
+	}
+}
+
---
+++ ns/org/wmaop/define/exception/registerExceptionMock/java.frag
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+
+<Values version="2.0">
+  <value name="name">registerExceptionMock</value>
+  <value name="encodeutf8">true</value>
+  <value name="body">bmV3IE1vY2tNYW5hZ2VyKCkucmVnaXN0ZXJFeGNlcHRpb24ocGlwZWxpbmUpOw==</value>
+</Values>
